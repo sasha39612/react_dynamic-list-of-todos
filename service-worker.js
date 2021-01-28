d39a005573e943a358ccf535b2610cd9ad684bf8
Oleksandr Stolyarov <Stolyarov_396@icloud.com>
2021-01-28 09:38:59 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.69c980133d8746ec7592bc9413534b1f.js"
+  "./precache-manifest.57d89122b4c59012ee4732c601fd51b4.js"
 );
 
 self.addEventListener('message', (event) => {
